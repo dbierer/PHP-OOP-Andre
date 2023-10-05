20fc92e621fcb352fe86b0351efc654806dbb9f5
OctoKing-dev <51433961+OctoKing-dev@users.noreply.github.com>
2023-10-05 00:08:35 -0700

    Add Person class

---
+++ relationbuilder/src/Entity/Person.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Person class
+ */
+namespace RelationBuilder\Entity;
+use RelationBuilder\Entity;
+
+class Person extends Entity {
+  public string firstName;
+  public string lastName;
+  
+  public function __construct($firstName, $lastName, $displayName) {
+    $this->firstName = $firstName;
+    $this->lastName = $lastName;
+    $this->displayName = $displayName ?? ($firstName . ($lastName ?? ''));
+  }
+  
+  public function getFullName() :string {
+    return trim(($this->firstName . ' ' ?? '') . ($this->lastName ?? ''));
+  }
+}
+
