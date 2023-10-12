77a7d1f5a09ae676790e8047dcd817d8c5f5525a
OctoKing-dev <51433961+OctoKing-dev@users.noreply.github.com>
2023-10-11 22:51:56 -0700

    Add magic methods and abstract getShortVersion to Entity

--- relationbuilder/src/RelationBuilder/Entity.php
+++ relationbuilder/src/RelationBuilder/Entity.php
@@ -4,11 +4,28 @@
  */
 namespace RelationBuilder;
 
-class Entity {
+abstract class Entity {
   public string $displayName;
+  public $customProperties = [];
   
   public function __construct($displayName) {
     $this->displayName = $displayName;
   }
+  
+  public function __get($property) {
+    if (array_key_exists($property, $this->customProperties)) {
+      return $this->customProperties[$property];
+    }
+  }
+  
+  public function __isset($property) {
+    return array_key_exists($property, $this->customProperties) && isset($this->customProperties[$property]);
+  }
+  
+  public function __tostring() {
+    return $this->displayName ?? get_class($this);
+  }
+  
+  abstract public function getShortVersion();
 }
 
