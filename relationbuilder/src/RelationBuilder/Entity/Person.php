a057402e9bf478443ff90a9c0e49395dd99fc42d
OctoKing-dev <51433961+OctoKing-dev@users.noreply.github.com>
2023-10-16 21:27:51 -0700

    Add additional type hinting

--- relationbuilder/src/RelationBuilder/Entity/Person.php
+++ relationbuilder/src/RelationBuilder/Entity/Person.php
@@ -9,7 +9,7 @@ class Person extends Entity {
   public string $firstName;
   public string $lastName;
   
-  public function __construct($firstName, $lastName, $displayName) {
+  public function __construct(string $firstName, string $lastName, ?string $displayName) {
     $this->firstName = $firstName;
     $this->lastName = $lastName;
     $this->displayName = $displayName ?? ($firstName . ' ' . ($lastName ?? ''));
